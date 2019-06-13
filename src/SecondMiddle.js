b7c7653f4c66400616912fa1649d31ea995d680d
Emilie Morissette <emorissettegregoire@gmail.com>
2019-06-13 11:24:51 +0200

    Added buttons

--- src/SecondMiddle.js
+++ src/SecondMiddle.js
@@ -5,7 +5,22 @@ export class SecondMiddle extends React.Component {
   render() {
     return (
       <div className="second-middle-style">
-        <h3>Style with... </h3>
+        <div className="second-middle-navbar">
+          <h3>Style with... </h3>
+          <div>
+            <a href="" className="add-to-cart-button">Add</a>
+            <a href="" className="remove-to-cart-button">Remove</a>
+          </div>
+        </div>
+
+       <div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+       </div>
+
+
       </div>
     );
   }
