4e74997676d61d7baadd8f757e256eec23c91686
Emilie Morissette <emorissettegregoire@gmail.com>
2019-06-14 12:14:19 +0200

    Done with the photos

--- src/SecondMiddle.js
+++ src/SecondMiddle.js
@@ -15,7 +15,6 @@ export class SecondMiddle extends React.Component {
         </div>
 
        <div className="accessories-wrapper">
-        <a href="#">
           <div className="accessory-1">
             <img src="https://www.visiofactory.com/70358/giorgio-armani-frames-of-life-yellow-matte-ar8093-5006-31-47-23-medium.jpg" alt="" />
             <div className="accessory-details">
@@ -23,10 +22,31 @@ export class SecondMiddle extends React.Component {
               <p>Reviews</p>
             </div>
           </div>
-        </a>
-        <div></div>
-        <div></div>
-        <div></div>
+
+          <div className="accessory-1">
+            <img src="http://www.lousadino.pt/64-large_default/zara-cropped-shirt.jpg" alt="" />
+            <div className="accessory-details">
+              <p>$175</p>
+              <p>Reviews</p>
+            </div>
+          </div>
+
+          <div className="accessory-1">
+            <img src="https://i.ebayimg.com/images/g/sgkAAOSwfr1bps2j/s-l1600.png" alt="" />
+            <div className="accessory-details">
+              <p>$175</p>
+              <p>Reviews</p>
+            </div>
+          </div>
+
+          <div className="accessory-1">
+            <img src="http://proscom-tn.com/wp-content/uploads/2019/03/466-600x662.png" alt="" />
+            <div className="accessory-details">
+              <p>$175</p>
+              <p>Reviews</p>
+            </div>
+          </div>
+
        </div>
 
 
