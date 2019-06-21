9181feed3e39d7711c5271a77e9be546c5745bdf
Nisha <nish_noor@yahoo.com>
2019-06-21 20:03:30 +0200

    UPDATE LEFT BANNER COMPONENT
    Updated left banner component

--- src/LeftBanner.js
+++ src/LeftBanner.js
@@ -2,6 +2,8 @@ import React from "react";
 import shoe1small from "../src/assets/images/blackShoe/shoe1small.jpg";
 import shoe2small from "../src/assets/images/blackShoe/shoe2small.jpg";
 import shoe3small from "../src/assets/images/blackShoe/shoe3small.jpg";
+import shoe4small from "../src/assets/images/blackShoe/shoe4small.jpg";
+import shoe5small from "../src/assets/images/blackShoe/shoe5small.jpg";
 
 export class LeftBanner extends React.Component {
   render() {
@@ -17,6 +19,8 @@ export class LeftBanner extends React.Component {
           <img src={shoe1small} alt="shoe image" />
           <img src={shoe2small} alt="shoe image" />
           <img src={shoe3small} alt="shoe image" />
+          <img src={shoe4small} alt="shoe image" />
+          <img src={shoe5small} alt="shoe image" />
         </div>
       </div>
     );
