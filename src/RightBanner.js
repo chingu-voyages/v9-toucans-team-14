355a3f166dd0957bf9a8b5455f1db0973cf0674b
Emilie Morissette <emorissettegregoire@gmail.com>
2019-06-24 12:54:06 +0300

    Added size circles but not designed yet

--- src/RightBanner.js
+++ src/RightBanner.js
@@ -1,11 +1,24 @@
 import React from "react";
+import "./App.css";
+import "./assets/stylesheets/components/banner/right-banner.css";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 export class RightBanner extends React.Component {
   render() {
     return (
       <div className="right-banner">
-        <h3>Right side to the black shoe</h3>
         <div className="size-wrapper">
+          <div>
+            <p> SIZE </p>
+          </div>
+          <div className="size-circles">
+            <p className="circle 37">37</p>
+            <p className="circle 38">38</p>
+            <p className="circle 39">39</p>
+            <p className="circle 40">40</p>
+            <p className="circle 41">41</p>
+          </div>
         </div>
         <div className="reviews-wrapper">
         </div>
