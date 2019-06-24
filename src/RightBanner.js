8b99c6bf4fa7d39647f77534d9998752cee3f498
Emilie Morissette <emorissettegregoire@gmail.com>
2019-06-24 12:38:53 +0300

    Created the divs

--- src/RightBanner.js
+++ src/RightBanner.js
@@ -5,6 +5,14 @@ export class RightBanner extends React.Component {
     return (
       <div className="right-banner">
         <h3>Right side to the black shoe</h3>
+        <div className="size-wrapper">
+        </div>
+        <div className="reviews-wrapper">
+        </div>
+        <div className="price-wrapper">
+        </div>
+        <div className="color-wrapper">
+        </div>
       </div>
     );
   }
