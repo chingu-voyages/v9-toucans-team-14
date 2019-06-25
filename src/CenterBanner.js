c3181ffa1cc99a9a642c10fe191ec2bcabd2a072
Nisha <nish_noor@yahoo.com>
2019-06-25 15:23:07 +0200

    STYLE LEFT CENTER FOOT BANNER
    Done styling left, center and foot banner

--- src/CenterBanner.js
+++ src/CenterBanner.js
@@ -1,11 +1,11 @@
 import React from "react";
-import shoe1big from "../src/assets/images/blackShoe/shoe1big.jpg";
+import shoe1big1 from "../src/assets/images/blackShoe/shoe1big1.jpg";
 
 export class CenterBanner extends React.Component {
   render() {
     return (
       <div className="center-banner">
-          <img src={shoe1big} alt="shoe image" />
+        <img src={shoe1big1} alt="shoe image" />
       </div>
     );
   }
--- src/FootBanner.js
+++ src/FootBanner.js
@@ -5,35 +5,36 @@ export class FootBanner extends React.Component {
       <div className="foot-banner">
         <div className="foot-button">
           <button className="play-button">
-            <img src="https://img.icons8.com/dotty/32/000000/circled-play.png" />
+            <img src="https://img.icons8.com/material/24/000000/circled-play.png" />
             play video
           </button>
           <div className="foot-arrow">
-            <img src="https://img.icons8.com/small/32/000000/long-arrow-left.png" />
-            <img src="https://img.icons8.com/small/32/000000/long-arrow-right.png" />
+            <img src="https://img.icons8.com/small/22/000000/long-arrow-left.png" />
+            <img src="https://img.icons8.com/small/22/000000/long-arrow-right.png" />
           </div>
-
-          <p>
+          <p className="lines">
             02
             <img
               className="first-line"
-              src="https://img.icons8.com/ios-glyphs/30/000000/horizontal-line.png"
+              src="https://img.icons8.com/ios-glyphs/22/000000/horizontal-line.png"
             />
             <img
               className="second-line"
-              src="https://img.icons8.com/ios-glyphs/30/000000/horizontal-line.png"
+              src="https://img.icons8.com/ios-glyphs/22/000000/horizontal-line.png"
             />
             <img
               className="third-line"
-              src="https://img.icons8.com/ios-glyphs/30/000000/horizontal-line.png"
+              src="https://img.icons8.com/ios-glyphs/22/000000/horizontal-line.png"
             />
             06
           </p>
-          <button className="add-cart">add to cart</button>
-          <img
-            className="heart"
-            src="https://img.icons8.com/windows/32/000000/like.png"
-          />
+          <div className="add-heart">
+            <button className="add-cart">add to cart</button>
+            <img
+              className="heart"
+              src="https://img.icons8.com/windows/18/000000/like.png"
+            />
+          </div>
         </div>
       </div>
     );
