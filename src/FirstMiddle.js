97e7b10edaa475ee889ac69b90396e4ec3d3bdca
Rodney <42125381+MthunziNamba@users.noreply.github.com>
2019-06-28 13:27:59 +0200

    Update FirstMiddle.js

--- src/FirstMiddle.js
+++ src/FirstMiddle.js
@@ -1,10 +1,91 @@
 import React from "react";
+import "./App.css";
+import "./assets/stylesheets/components/first-middle.css";
+
 
 export class FirstMiddle extends React.Component {
   render() {
     return (
       <div className="first-middle-black">
-        <h3>Made of black technical knit fabric</h3>
+        <div className="first-middle-left-navbar">
+          <h3>Made of black <br></br>technical knit fabric</h3>
+        </div>
+        <div className = "first-middle-right-navbar" >
+          <h4>Reviews</h4> 
+          </div>
+        <div className="left-description">
+          <div className="description">
+            <ul>
+              <li>
+                <h4>Description</h4>
+                <p> Running sneakers with thin elastic laces.Wavy rubber sole.Made<br></br> of black technical knit fabric.The rubberized film details highlight <br></br> the shape of the shoe.Customized with detachable rubber tab <br></br>appliqué on the heel.
+                </p>
+              </li>
+            </ul>
+          </div>
+          </div>
+          <div className="composition-box">
+          <div className="composition">  
+            <ul>  
+              <li>
+                <h4>Composition</h4>
+                <p>80% polyamide, 20% elastane,<br></br> 100% calf leather, inside: 100%<br></br> polyester.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="right-description">
+          <div className="reviews">
+            <ul>
+              <li>
+                <p className="name-tag"><img src = "./images/first-middle/David-Heart.jpg"
+                alt=""/><strong>David Heart</strong> - June 06, 2019</p>
+                <p className="review-content">Blogging twice a week, catching up with other people 's writing getting back to...
+                </p>
+              </li>
+              </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <ul>
+              <li>
+                <p className="name-tag"><img src = "./images/first-middle/Kim-North.jpg"
+                alt = ""/><strong>Kim North</strong>- May 15, 2019 </p>
+                <p className="review-content">I went through a rollercoaster of contradictory emotions: skepticism...
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div>
+          <div>
+            <ul>
+              <li>
+                
+                <p className = "name-tag"><img src = "./images/first-middle/Lisa-Peters.jpg"
+                alt = ""
+                width = "28"
+                height = "28"/><strong>Lisa Peters</strong> - January 21, 2019</p >
+                <p className="review-content"> Realization, happiness, guilt, elation, excitement, curiosity.I remember you.
+                </p>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className = "socials">
+          <li>
+          <a href = "#" class="fa fa-facebook"></a> 
+          <a href="#" class = "fa fa-twitter" ></a>
+          <a href = "#" class = "fa fa-instagram" ></a> 
+          </li> 
+        </div>
+        <div className = "next-buttons" >
+          <span className = "fa fa-long-arrow-left" ></span> 
+          <span className = "fa fa-long-arrow-right" ></span>
+          <p className="number-page">45-50</p>
+        </div>
       </div>
     );
   }
