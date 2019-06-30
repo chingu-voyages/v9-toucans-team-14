073572c31217609054da9edc0e2a20c76ae4bb50
Emilie Morissette <emorissettegregoire@gmail.com>
2019-06-30 11:04:47 +0300

    Not done with the logic on toggeling classes

--- src/SizeButton.js
+++ src/SizeButton.js
@@ -6,9 +6,11 @@ export class SizeButton extends React.Component {
     this.state = {clicked: false}
   };
 
+
+
   handleClick = () => {
     this.setState({
-    clicked: !this.state.clicked
+    clicked: !this.state.clicked,
   });
   }
 
@@ -18,10 +20,11 @@ export class SizeButton extends React.Component {
             <p className={`circle s37 ${this.state.clicked ? 'selected': null}`}
             onClick={this.handleClick}>
             <strong>37</strong></p>
-            <p className={`circle s38 ${this.state.clicked ? 'selected': null}`}
+            <p className={`circle s38 ${this.state.clicked ? 'selected' : null}`}
             onClick={this.handleClick}>
             <strong>38</strong></p>
-            <p className="circle s39"><strong>39</strong></p>
+            <p className={`circle s39 ${this.state.clicked ? 'selected' : null}`}
+            onClick={this.handleClick}><strong>39</strong></p>
             <p className="circle s40 na"><strong>40</strong></p>
             <p className="circle s41"><strong>41</strong></p>
       </div>
