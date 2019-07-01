c0743fae475e7d68d098a3496cc7c514dd99c6f6
Nisha <nish_noor@yahoo.com>
2019-07-01 09:55:47 +0200

    UPDATE CENTER-BANNER

--- src/CenterBanner.js
+++ src/CenterBanner.js
@@ -1,5 +1,10 @@
 import React from "react";
 import shoe1big1 from "../src/assets/images/blackShoe/shoe1big1.jpg";
+import yellowLarge1 from "../src/assets/images/yellowShoe/yellowLarge1.jpg";
+import yellowLarge2 from "../src/assets/images/yellowShoe/yellowLarge2.jpg";
+import yellowLarge3 from "../src/assets/images/yellowShoe/yellowLarge3.jpg";
+import yellowLarge4 from "../src/assets/images/yellowShoe/yellowLarge4.jpg";
+import yellowLarge5 from "../src/assets/images/yellowShoe/yellowLarge5.jpg";
 
 export class CenterBanner extends React.Component {
   render() {
--- src/YellowShoeImages.js
+++ src/YellowShoeImages.js
@@ -4,11 +4,6 @@ import yellowSmall2 from "../src/assets/images/yellowShoe/yellowSmall2.jpg";
 import yellowSmall3 from "../src/assets/images/yellowShoe/yellowSmall3.jpg";
 import yellowSmall4 from "../src/assets/images/yellowShoe/yellowSmall4.jpg";
 import yellowSmall5 from "../src/assets/images/yellowShoe/yellowSmall5.jpg";
-import yellowLarge1 from "../src/assets/images/yellowShoe/yellowLarge1.jpg";
-import yellowLarge2 from "../src/assets/images/yellowShoe/yellowLarge2.jpg";
-import yellowLarge3 from "../src/assets/images/yellowShoe/yellowLarge3.jpg";
-import yellowLarge4 from "../src/assets/images/yellowShoe/yellowLarge4.jpg";
-import yellowLarge5 from "../src/assets/images/yellowShoe/yellowLarge5.jpg";
 
 export default function YellowShoeImages() {
   return (
