5cda859f24beda1184f36a355c8abbc1a49985f8
Emilie Morissette <emorissettegregoire@gmail.com>
2019-07-04 10:13:36 +0300

    Changed the <p> tags for <a> tags for the size button

--- src/SizeButton.js
+++ src/SizeButton.js
@@ -1,36 +1,145 @@
 import React from "react";
 
+// export class SizeButton extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       clicked: true,
+//     }
+//   };
+
+//   handleClick = () => {
+//     this.setState({
+//     clicked: !this.state.clicked
+//     });
+//   }
+
+//   render() {
+//     return (
+//       <div className="size-circles">
+//             <p className={`circle s37 ${this.state.clicked ? null : 'selected'}`}
+//             onClick={this.handleClick}>
+//             <strong>37</strong></p>
+//             <p className="circle s38"
+//             onClick={this.handleClick}>
+//             <strong>38</strong></p>
+//             <p className="circle s39"
+//             onClick={this.handleClick}><strong>39</strong></p>
+//             <p className="circle s40 na"
+//             OnClick={this.handleClick}><strong>40</strong></p>
+//             <p className="circle s41"
+//             onClick={this.handleClick} ><strong>41</strong></p>
+//       </div>
+//     );
+//   }
+// }
+
+
+// import React, { Component } from 'react';
+
+// const { Component } = React;
+
 export class SizeButton extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      clicked: true,
+  get classNames() {
+    if (this.props.clicked) {
+      return 'selected'
     }
-  };
 
-  handleClick = () => {
-    this.setState({
-    clicked: !this.state.clicked
-    });
+    return ""
   }
 
   render() {
+    const { children, onClick } = this.props;
     return (
-      <div className="size-circles">
-            <p className={`circle s37 ${this.state.clicked ? null : 'selected'}`}
-            onClick={this.handleClick}>
-            <strong>37</strong></p>
-            <p className="circle s38"
-            onClick={this.handleClick}>
-            <strong>38</strong></p>
-            <p className="circle s39"
-            onClick={this.handleClick}><strong>39</strong></p>
-            <p className="circle s40 na"
-            OnClick={this.handleClick}><strong>40</strong></p>
-            <p className="circle s41"
-            onClick={this.handleClick} ><strong>41</strong></p>
-      </div>
+
+      <div className={`size-circles ${this.classNames}`}
+          type="button"
+          onClick={onClick}>
+          {children}
+          <a href="#" className="circle s37"><strong>37</strong></a>
+          <a href="#" className="circle s38"><strong>38</strong></a>
+          <a href="#" className="circle s39"><strong>39</strong></a>
+          <a href="#" className="circle s40 na"><strong>40</strong></a>
+          <a href="#" className="circle s41"><strong>41</strong></a>
+     </div>
     );
   }
 }
+
+// class App extends Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       buttons: [
+//         {
+//           id: 1,
+//           name: 'Button 1',
+//           clicked: true
+//         },
+//         {
+//           id: 2,
+//           name: 'Button 2',
+//           clicked: false
+//         },
+//         {
+//           id: 3,
+//           name: 'Button 3',
+//           clicked: false
+//         },
+//         {
+//           id: 4,
+//           name: 'Button 4',
+//           clicked: false
+//         },
+//       ]
+//     };
+//   }
+
+//   handleClicked(id) {
+//     this.setState(prevState => {
+//       const buttons = prevState.buttons.map(
+//         (button) => {
+//           if (button.id === id) {
+//             return { ...button, clicked: true }
+//           }
+
+//           return { ...button, clicked: false }
+//         }
+//       );
+
+//       // same as { buttons: buttons }
+//       return { buttons }
+//     })
+//   }
+
+//   /**
+//    * Getter method, Read Only
+//    */
+//   get buttons() {
+//     const { buttons } = this.state;
+
+//     return buttons.map(
+//       ({ name, clicked, id}) => (
+//         <Button
+//           key={id}
+//           clicked={clicked}
+//           onClick={
+//             () => this.handleClicked(id)
+//           }
+//         >
+//           {name}
+//         </Button>
+//       )
+//     )
+//   }
+
+//   render() {
+//     return(
+//       <div>
+//         {this.buttons}
+//       </div>
+//     );
+//   }
+// }
