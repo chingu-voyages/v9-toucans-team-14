8545887823e16c0dfc79c1144626e2a053691d2a
Emilie Morissette <emorissettegregoire@gmail.com>
2019-07-05 14:30:37 +0300

    Fixed an issue

--- src/ColorButton.js
+++ src/ColorButton.js
@@ -1,21 +1,23 @@
 import React from 'react';
 
-export class ColorButton extends React.component {
+export class ColorButton extends React.Component {
   get classNames() {
     const { clicked, name } = this.props;
 
     if (clicked) {
-      return `btn-color-{$name} active`;
+      return `btn-color-${name} active`;
     }
 
-    return `btn-color-{$name}`;
+    return `btn-color-${name}`;
   }
 
   render() {
     const { onClick, name } = this.props;
     return (
-      <button type="button" className={this.classNames} onClick={onClick} >
+      <button type="button" className={this.classNames} onClick={onClick}>
         {name}
-      </button>)
+      </button>
+
+    );
   }
 }
