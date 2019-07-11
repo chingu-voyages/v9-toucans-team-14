62aad5585c25ae6c69b9274100cd7ef1ba453646
Emilie Morissette <emorissettegregoire@gmail.com>
2019-07-11 17:20:56 +0300

    commit

---
+++ src/ColorButtons.js
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import { ColorButton } from './ColorButton';
+
+export class ColorButtons extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      colorbuttons: [
+        {
+          id: 1,
+          name: ['black'],
+          clicked: false
+        },
+        {
+          id: 2,
+          name: ['white'],
+          clicked: true
+        }
+      ]
+    };
+  }
+
+  handleClicked(id) {
+    this.setState(prevState => {
+      const colorbuttons = prevState.colorbuttons.map(colorbutton => {
+        if (colorbutton.id === id) {
+          return { ...colorbutton, clicked: true };
+        }
+
+        return { ...colorbutton, clicked: false };
+      });
+
+      // same as { buttons: buttons }
+      return { colorbuttons };
+    });
+  }
+
+  /**
+   * Getter method, Read Only
+   */
+  get colorbuttons() {
+    const { colorbuttons } = this.state;
+
+    return colorbuttons.map(({ id, colors, clicked }) => (
+      <ColorButton
+        key={id}
+        colors={colors}
+        clicked={clicked}
+        onClick={() => this.handleClicked(id)}
+      />
+    ));
+  }
+
+  render() {
+    return <div className="colour-btns-set1">{this.colorbuttons}</div>;
+
+  }
+}
